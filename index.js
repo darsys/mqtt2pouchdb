94e29da6019294f966220b12b4c3a50008ac6f2c
darsys <damon@ourcomputershop.com>
2019-09-14 01:04:20 -0400

    updated for service operations

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://localhost')
+var mqttServer = 'mqtt://localhost'
+var client  = mqtt.connect(mqttServer)
 
 var PouchDB = require('pouchdb')
 var db = new PouchDB('http://admin:taytob01@localhost:5984/vanpower')
 
 client.on('connect', function () {
-    client.subscribe('vanpower', function (err) {
+  console.log("Connected to " + mqttServer)
+  var aTopic = 'vanpower'
+  client.subscribe(aTopic, function (err) {
       if (!err) {
-        client.publish('presence', 'Hello mqtt')
+        console.log("Subscribed to " + aTopic)
+        // client.publish('presence', 'Hello mqtt')
+      }
+      else {
+        console.log("Subscription error:" + err)
+        client.end()
       }
     })
   })
    
   client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
+    console.log('Topic [' + topic + '] updated')
+    //console.log(message.toString())
     var myDoc = message.toJSON()
     myDoc['_id'] = new Date().toISOString()
     db.put(myDoc, function callback(err, result) {
-      if (!err) { console.log('db record sent')}
-      else {console.log(err.message)}
+      if (err) {
+        console.log(err.message)
+      }
+      else {
+        console.log('couchDB doc created')
+      }
     });
     //client.end()
   })
